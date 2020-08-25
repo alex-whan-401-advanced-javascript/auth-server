8b90bf5320aa9fb6f4deb2b2d1592553d504e8fb
Alexander Whan <whan.alexander@gmail.com>
2020-08-24 21:46:23 -0700

    Set up index.js entry point, connected server, required mongoose

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const server = require('./src/server');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
+
+server.start(process.env.PORT);
