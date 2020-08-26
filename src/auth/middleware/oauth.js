47ac90b522335d33e93b070828c3cf0e09c56893
Alexander Whan <whan.alexander@gmail.com>
2020-08-25 23:15:16 -0700

    Created OAuth methods and structure

--- src/auth/middleware/oauth.js
+++ src/auth/middleware/oauth.js
@@ -16,18 +16,63 @@ module.exports = async function authorize(req, res, next) {
   try {
     let code = req.query.code;
     console.log('(1) CODE: ', code);
+
+    let remoteToken = await exchangeCodeForToken(code);
+    console.log('(2) ACCESS TOKEN: ', remoteToken);
+
+    let remoteUser = await getRemoteUserInfo(remoteToken);
+    console.log('(3) GITHUB USER: ', remoteUser);
+
+    let [user, token] = await getUser(remoteUser);
+    // Add the token and the user record to the request object
+    req.user = user;
+    req.token = token;
+    console.log('(4) LOCAL USER: ', user);
+    // If it is successful, use next() to continue on to the actual route handler
+    next();
   } catch (error) {
+    // If not, the middleware should invoke the error handler by calling next() with an error
     next(`ERROR: ${error.message}`);
   }
 };
 
-// This should be required by your auth router and attached inline to the /oauth route:
-// app.get('/oauth', OAuthMiddleware, ...)
-// This middleware will need to do the following:
 // Exchange the code received on the initial request for a token from the Provider
+async function exchangeCodeForToken(code) {
+  let tokenResponse = await (await superagent.post(tokenServerUrl)).send({
+    code: code,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    redirect_uri: API_SERVER,
+    grant_type: 'authorization code',
+  });
+
+  let access_token = tokenResponse.body.access_token;
+
+  return access_token;
+}
+
 // Use the token to retrieve the user’s account information from the Provider
+async function getRemoteUserInfo(token) {
+  let userResponse = await superagent
+    .get(remoteAPI)
+    .set('user-agent', 'express-app')
+    .set('Authorization', `token ${token}`);
+
+  let user = userResponse.body;
+
+  return user;
+}
+
 // Create/Retrieve an account from our Mongo users database matching the user’s account (email or username) using the users model
-// Generate a token using the users model
-// Add the token and the user record to the request object
-// If it is successful, use next() to continue on to the actual route handler
-// If not, the middleware should invoke the error handler by calling next() with an error
+async function getUser(remoteUser) {
+  let userRecord = {
+    username: remoteUser.login,
+    password: 'oauthpassword',
+  };
+
+  let user = await users.save(userRecord);
+  // Generate a token using the users model
+  let token = users.generateToken(user);
+
+  return [user, token];
+}
