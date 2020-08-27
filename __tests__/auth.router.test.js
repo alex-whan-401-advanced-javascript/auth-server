0df2a30a20e2665e26132b05e5a15f93e69d789a
Alexander Whan <whan.alexander@gmail.com>
2020-08-26 19:08:30 -0700

    Refactored router test suites to be more concise/informative

--- __tests__/auth.router.test.js
+++ __tests__/auth.router.test.js
@@ -1,85 +1,3 @@
-// 'use strict';
-
-// process.env.SECRET = 'muysecreto';
-
-// const jwt = require('jsonwebtoken');
-// require('dotenv').config();
-// const server = require('../src/server').server;
-// const supergoose = require('@code-fellows/supergoose');
-
-// const mockRequest = supergoose(server);
-
-// describe('Auth Router', () => {
-//   describe(`users signup/in`, () => {
-//     it('can sign up', async () => {
-//       const userData = {
-//         username: 'admin',
-//         password: 'password',
-//         role: 'admin',
-//         email: 'admin@admin.com',
-//       };
-
-//       const results = await mockRequest.post('/signup').send(userData);
-
-//       expect(results.statusCode).toBe(201);
-//     });
-
-//     it('can signin with basic', async () => {
-//       const userData = {
-//         username: 'joey',
-//         password: 'password',
-//         role: 'admin',
-//         email: 'admin@admin.com',
-//       };
-
-//       await mockRequest.post('/signup').send(userData);
-
-//       const results = await mockRequest
-//         .post('/signin')
-//         .auth('joey', 'password');
-
-//       let parsedToken = JSON.parse(results.text).token;
-//       const token = jwt.verify(parsedToken, process.env.SECRET);
-
-//       expect(token).toBeDefined();
-//     });
-
-//     it.skip('can fail signin with bad password', async () => {
-//       const userData = {
-//         username: 'joey',
-//         password: 'password',
-//         role: 'admin',
-//         email: 'admin@admin.com',
-//       };
-
-//       await mockRequest.post('/signup').send(userData);
-
-//       const results = await mockRequest
-//         .post('/signin')
-//         .auth('joey', 'badpassword');
-
-//       expect(results.statusCode).toBe(401);
-//     });
-
-//     it.skip('can fail signin with unknown user', async () => {
-//       const userData = {
-//         username: 'joey',
-//         password: 'password',
-//         role: 'admin',
-//         email: 'admin@admin.com',
-//       };
-
-//       await mockRequest.post('/signup').send(userData);
-
-//       const results = await mockRequest
-//         .post('/signin')
-//         .auth('nobody', 'password');
-
-//       expect(results.statusCode).toBe(401);
-//     });
-//   });
-// });
-
 'use strict';
 
 require('dotenv').config();
@@ -87,9 +5,6 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const { server } = require('../src/server');
-
-// Alternatively:
-// const server = require("../../../src/app.js").server;
 
 const supergoose = require('@code-fellows/supergoose');
 
