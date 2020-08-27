ec98b85111d5f5632c4621d581399a31e633aaa7
Alexander Whan <whan.alexander@gmail.com>
2020-08-26 19:23:43 -0700

    Added basic error handler 404/500 middleware

--- src/server.js
+++ src/server.js
@@ -4,12 +4,18 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const authRouter = require('../src/auth/router');
+const notFound = require('../src/middleware/404');
+const errorHandler = require('../src/middleware/500');
 
 // Global Middleware
 app.use(express.json());
 
 // Routes
 app.use(authRouter);
+
+// Catchalls/Errors
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = {
   server: app,
