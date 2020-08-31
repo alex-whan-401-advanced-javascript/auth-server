5066ca06cefed968abfc00116f844ed6daada623
Alexander Whan <whan.alexander@gmail.com>
2020-08-31 08:32:26 -0700

    Skipped access control tests for now

--- __tests__/extra-routes.test.js
+++ __tests__/extra-routes.test.js
@@ -8,18 +8,20 @@ const supergoose = require('@code-fellows/supergoose');
 
 const mockRequest = supergoose(server);
 
-it('should allow entry with good token', async () => {
-  // Can be convenient to store a TEST_TOKEN in environment
-  // But you will have to refresh it (aka grab a new one) if/when it expires
-  const response = await mockRequest
-    .get('/secret')
-    .auth(process.env.TEST_TOKEN, { type: 'bearer' });
-  expect(response.status).toBe(200);
-});
+describe.skip('Extra routes tests', () => {
+  it('should allow entry with good token', async () => {
+    // Can be convenient to store a TEST_TOKEN in environment
+    // But you will have to refresh it (aka grab a new one) if/when it expires
+    const response = await mockRequest
+      .get('/secret')
+      .auth(process.env.TEST_TOKEN, { type: 'bearer' });
+    expect(response.status).toBe(200);
+  });
 
-it('should NOT allow entry with bad token', async () => {
-  const response = await mockRequest
-    .get('/secret')
-    .auth('bad token', { type: 'bearer' });
-  expect(response.status).toBe(500);
+  it('should NOT allow entry with bad token', async () => {
+    const response = await mockRequest
+      .get('/secret')
+      .auth('bad token', { type: 'bearer' });
+    expect(response.status).toBe(500);
+  });
 });
--- src/extra-routes.js
+++ src/extra-routes.js
@@ -5,13 +5,13 @@ const bearerAuth = require('./auth/middleware/bearer');
 const router = express.Router();
 const permissions = require('./auth/middleware/acl');
 
-router.get('/secret', bearerAuth, (req, res) => {
-  res.status(200).send('SUCCESS! Access granted.');
-});
+// router.get('/secret', bearerAuth, (req, res) => {
+//   res.status(200).send('SUCCESS! Access granted.');
+// });
 
 // Where needed, we pass in the desired "permission" to the ACL middleware - which then matches it to the desire "capability"/CRUD ability in the User Model
 router.get('/public', routeHandler);
-router.get('./private', bearerAuth, routeHandler);
+router.get('/private', bearerAuth, routeHandler);
 router.get('/readonly', bearerAuth, permissions('read'), routeHandler);
 router.post('/create', bearerAuth, permissions('create'), routeHandler);
 router.post('/update', bearerAuth, permissions('update'), routeHandler);
